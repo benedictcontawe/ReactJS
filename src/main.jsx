826c753f397b51ff16cd7022a9bf80c9301220f5
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-21 18:29:28 +0800

    added Card

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Card from './compose/Card.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Card />
   </StrictMode>,
 )
