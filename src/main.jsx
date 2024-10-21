a1f8e2542527139069c34a07d934495144e78d52
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-21 20:01:50 +0800

    New branch for Task Application Creating Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Card from './compose/Card.jsx'
-import CreateTodo from './compose/CreateTodo.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <Card /> */}
-    <CreateTodo />
+    <App />
   </StrictMode>,
 )
