cb642d18b03a2b9725c24d658119122258b03a47
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-21 18:30:08 +0800

    Added Create Todo

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Card from './compose/Card.jsx'
+import CreateTodo from './compose/CreateTodo.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Card />
+    {/* <Card /> */}
+    <CreateTodo />
   </StrictMode>,
 )
