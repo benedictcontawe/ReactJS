90ba4f18da4121313099a95b3c3113d3c749bea9
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-22 22:32:04 +0800

    update TaskColumn props Object Destructuring

--- src/compose/TaskColumn.jsx
+++ src/compose/TaskColumn.jsx
@@ -4,10 +4,11 @@ import "./TaskColumn.css";
 
 const TaskColumn = (props) => {
     console.log("TaskColumn props", props)
+    const {text, icon} = props
     return (
         <section className='task_column'>
             <h2 className="task_column_heading">
-                <img className="task_column_icon" src={props.icon} alt="" /> {props.text}
+                <img className="task_column_icon" src={icon} alt="" /> {text}
             </h2>
         </section>
     )
