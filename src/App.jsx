04adc64edad74cbeaaf0c02e1ebb8054b538ce36
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-24 19:20:39 +0800

    added support for localStorage with useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import TaskForm from './components/TaskForm';
 import TaskColumn from './components/TaskColumn';
 import Todo from "./assets/direct-hit.png";
 import Doing from "./assets/glowing-star.png"
 import Done from "./assets/check-mark-button.png"
+const oldTasks = localStorage.getItem("tasks")
+console.log("App", "oldTasks", oldTasks)
 
 const App = () => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks])
   const handleDelete = (taskIndex) => {
     const newTasks = tasks.filter((task, index) => index !== taskIndex);
     setTasks(newTasks);
-  };
+  }; 
   console.log("App", "tasks", tasks); 
   return (
     <div className='app'>
