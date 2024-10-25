35d91c7287f64166a4c8528f61f4fdb7728b33ad
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-25 17:33:23 +0800

    Added MovieList Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import MovieList from './components/MovieList/MovieList'
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
-      <main>Main Content</main>
+      <MovieList />
     </div>
   )
 }
