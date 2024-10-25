a0ba3b3e02485af1bc7504bc91d04abe83c6513b
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-25 16:52:58 +0800

    Added Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className='app'>
-      <nav className='navbar'>Navbar content</nav>
+      <Navbar />
       <main>Main Content</main>
     </div>
   )
