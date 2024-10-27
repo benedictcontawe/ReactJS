8e28ea122b4f9770f23121541f3e2ac71d546ffa
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-27 16:20:44 +0800

    Move Ratings to Filter Group

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './MovieList.css';
 import Fire from '../../assets/fire.png'
 import MovieCard from './MovieCard';
+import FilterGroup from './FilterGroup';
 
 const MovieList = () => {
     const [movies, setMovies] = useState([])
@@ -35,7 +36,7 @@ const MovieList = () => {
             setFilterMovies(filtered)
         }
     }
-
+    
   return (
     <section className='movie_list'>
         <header className='align_center movie_list_header'>
@@ -44,11 +45,7 @@ const MovieList = () => {
                 <i mg src={Fire} alt='fire emoji' className='navbar_emoji' />
             </h2>
             <div className='align_center movie_list_fs' >
-                <ul className="align_center  movie_filter">
-                    <li className={minRating === 8 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(8)}>8+ Star</li>
-                    <li className={minRating === 7 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(7)}>7+ Star</li>
-                    <li className={minRating === 6 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(6)}>6+ Star</li>
-                </ul>
+                <FilterGroup minRating={minRating} onRatingClick={handleFilter} ratings={[8, 7, 6]} />
                 <select name="" id="" className="movie_sorting">
                     <option value="">SortBy</option>
                     <option value="">Date</option>
