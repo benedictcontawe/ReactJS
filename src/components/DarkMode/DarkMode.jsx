ab90359d0058c55865460dd7293ee532ef211ce1
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-27 20:41:59 +0800

    Added Dark and Light Theme UI UX Toggle Functionality

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -4,13 +4,39 @@ import "./DarkMode.css";
 import Sun from '../../assets/Sun.svg?react' //import Sun from './Sun.svg?react'; import { ReactComponent as Sun } from "./Sun.svg";
 import Moon from '../../assets/Moon.svg?react' //import Moon from './Moon.svg?react'; import { ReactComponent as Moon } from "./Moon.svg";
 
-const  DarkMode = () => {
+const DarkMode = () => {
+    const setDarkTheme = () => {
+        document.querySelector("body").setAttribute("data-theme", "dark");
+        localStorage.setItem("selectedTheme", "dark");
+    }
+    const setLightTheme = () => {
+        document.querySelector("body").setAttribute("data-theme", "light");
+        localStorage.setItem("selectedTheme", "light");
+    }
+    const selectedTheme = localStorage.getItem("selectedTheme");
+    if(selectedTheme === "light") {
+        setLightTheme();
+    } else {
+        setDarkTheme();
+    }
+    const toggleTheme = event => {
+        event.target.checked ? setDarkTheme() : setLightTheme();
+        /*
+        if (event.target.checked) {
+            setDarkTheme();
+        } else {
+            setLightTheme();
+        }
+        */
+    }
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
+                onChange={toggleTheme}
+                defaultChecked={ selectedTheme !== "light" }
             />
             <label className='dark_mode_label' htmlFor='darkmode-toggle'>
                 <Sun />
