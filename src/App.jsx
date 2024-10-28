10bc7151148107ace5bd9c7b9f155a862fdc7d77
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-28 23:10:54 +0800

    Route Parameters movie id

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Star from './assets/glowing-star.png'
 import Party from './assets/partying-face.png'
 import Navbar from './components/Navbar/Navbar'
 import MovieList from './components/MovieList/MovieList'
+import SingleMovie from './components/MovieList/SingleMovie';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/popular" element={<MovieList type="popular" title="Popular" emoji={Fire} />} />
             <Route path="/top_rated" element={<MovieList type="top_rated" title="Top Rated" emoji={Star} />} />
             <Route path="/upcoming" element={<MovieList type="upcoming" title="Upcoming" emoji={Party} />} />
+            <Route path='/movie/:movieId' element={<SingleMovie />} />
         </Routes>
       </main>
     </div>
--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './MovieCard.css'
 import Star from '../../assets/star.png'
 
 const MovieCard = ({movie}) => {
   return (
-    <a href={`https://www.themoviedb.org/movie/${movie.id}`} target='_blank' className='movie_card'>
+    <Link to={`/movie/${movie.id}`} className='movie_card'>
         <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt='movie poster' className='movie_poster' />
         <div className="movie_details">
             <h3 className='movie_details_heading'>
@@ -21,7 +22,7 @@ const MovieCard = ({movie}) => {
                 { movie.overview }
             </p>
         </div>
-    </a>
+    </Link>
   )
 }
 
