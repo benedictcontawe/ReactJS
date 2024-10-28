9732a46d9f38e9f353516d0d206dc3c760156d08
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-28 11:00:43 +0800

    new Branch for Routing Features

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Card from './compose/Card.jsx'
-import CreateTodo from './compose/CreateTodo.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-    {/* <Card /> */}
-    <CreateTodo />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
