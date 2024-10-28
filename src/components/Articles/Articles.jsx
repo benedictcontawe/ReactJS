1e0cfbabeb8d72b9dcc157aa8f070eb5e37d0a46
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-28 21:18:16 +0800

    router query string

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Articles = () => {
+    const [ searchParams, setSearchParams ] = useSearchParams()
+    const sortBy = searchParams.get("sortBy")
+    const category = searchParams.get("category")
+    const handleSortBy = () => {
+        setSearchParams({
+            sortBy: "views",
+            category: "electronics",
+        });
+    }
     return (
         <div>
             <h2>
                 Articles
-                <p>SortBy: Category:</p>
+                <p>
+                    SortBy: {sortBy} Category: {category}
+                </p>
             </h2>
+            <button onClick={handleSortBy}>Sort by views</button>
         </div>
     );
 };
