fb169b029bca9635d7fef4a5c216c44c57a9b1f8
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-28 22:36:57 +0800

    Navigate and useNavigate feature for back and redirect when not admin

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 
 const Admin = () => {
+    const user = {role: "user"}
+    if(user.role !== "admin") {//Navigate to Home when not admin
+        return <Navigate to='/'/>
+    }
     return (
         <div>
             <h2>Admin Panel</h2>
--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SingleProduct = () => {
     const { id } = useParams();
     console.log("SingleProduct", "params", id)
+    const navigate = useNavigate()
+    const handleBack = () => {
+        console.log("SingleProduct", "handleBack",)
+        navigate(-1)
+    }
     return (
         <div>
             <h2>SingleProduct - {id} </h2>
+            <button onClick={handleBack}>Go Back</button>
         </div>
     );
 };
