911cb36faeefc5a2b458bdb059157416b3c25ad4
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-28 23:31:50 +0800

    make user role admin use of side effect with title once mount in Sales and per input change in Sellers Title

--- src/components/Admin/Sales.jsx
+++ src/components/Admin/Sales.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect }from "react";
 
 const Sales = () => {
+    useEffect( () => {
+        console.log("Sales Component Mount!")
+        const notification = 5;
+        document.title = `Sales ${notification} new Notification`
+    }, [])
     return <h3>Admin Sales Page</h3>;
 };
 
--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Sellers = () => {
-    return <h3>Admin Sellers Page</h3>;
+    const [name, setName] = useState("");
+    useEffect( () => {
+        const notification = 5;
+        document.title = `Seller ${name}`
+    }, [name])
+    return (
+        <>
+        <h3>Admin Sellers Page</h3>
+        <input type="text" onChange={(e) => setName(e.target.value)}></input>
+        </>
+    );
 };
 
 export default Sellers;
