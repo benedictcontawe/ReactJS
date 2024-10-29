6aa4e99c920b400399b91ed674447766c66708f8
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-29 11:46:55 +0800

    added axios fet method

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -1,15 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Sellers = () => {
     const [name, setName] = useState("");
+    const [sellers, setSellers] = useState([])
     useEffect( () => {
-        const notification = 5;
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+            console.log("Sellers", "axios.get", response)
+            setSellers(response.data)
+        })
         document.title = `Seller ${name}`
+        return () => {
+            console.log("Sellers", "Component Unmount")
+        }
     }, [name])
     return (
         <>
         <h3>Admin Sellers Page</h3>
         <input type="text" onChange={(e) => setName(e.target.value)}></input>
+        {
+            sellers.map(seller => 
+                <p key={seller.id}>{seller.name}</p>
+            )
+        }
         </>
     );
 };
