025942148fccef63905847ec730428190338a052
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-29 12:49:28 +0800

    handling API Errors

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -5,16 +5,23 @@ import Loader from "../Common/Loader";
 const Sellers = () => {
     const [name, setName] = useState("");
     const [isLoading, setIsLoading] = useState(false)
+    const [errors, setErrors] = useState("")
     const [sellers, setSellers] = useState([])
     useEffect( () => {
         setIsLoading(true)
         axios.get("https://jsonplaceholder.typicode.com/users")
         .then((response) => {
-            console.log("Sellers", "axios.get", response)
-            setSellers(response.data)
-            setIsLoading(false)
+            console.log("Sellers", "axios.get", response);
+            setSellers(response.data);
+            setIsLoading(false);
+        }).catch((error) => {
+            console.log("Sellers", "axios.get", "catch error", error);
+            setIsLoading(false);
+            setErrors(error.message);
         })
-        document.title = `Seller ${name}`
+    }, [])
+    useEffect(() => {
+      document.title = `Seller ${name}`
         return () => {
             console.log("Sellers", "Component Unmount")
         }
@@ -25,6 +32,7 @@ const Sellers = () => {
         <h3>Admin Sellers Page</h3>
         <input type="text" onChange={(event) => setName(event.target.value)}></input>
         { isLoading && <Loader/> }
+        { errors && <em>{errors}</em> }
         {
             sellers.map( seller => 
                 <p key={seller.id}>{seller.name}</p>
