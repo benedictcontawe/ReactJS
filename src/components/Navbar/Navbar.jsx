deb06abd4b56afaf47c0745a1e2056cb82f6e05d
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-29 18:57:26 +0800

    add Navbar Links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
+import rocket from '../../assets/rocket.png';
+import star from '../../assets/glowing-star.png';
+import idButton from '../../assets/id-button.png';
+import memo from '../../assets/memo.png';
+import order from '../../assets/package.png';
+import lock from '../../assets/locked.png';
+import LinkWithIcon from './LinkWithIcon';
 
 const Navbar = () => {
   return (
@@ -11,8 +18,16 @@ const Navbar = () => {
                 <button type='submit' className='search_button' >Search</button>
             </form>
         </div>
-        <div>
-            
+        <div className='align_center navbar_links'>
+            <LinkWithIcon title="Home" link="/" emoji={rocket}/>
+            <LinkWithIcon title="Products" link="/products" emoji={star}/>
+            <LinkWithIcon title="LogIn" link="/login" emoji={idButton}/>
+            <LinkWithIcon title="SignUp" link="/signup" emoji={memo}/>
+            <LinkWithIcon title="My Orders" link="/myorders" emoji={order}/>
+            <LinkWithIcon title="Logout" link="/logout" emoji={lock}/>
+            <a href="/cart" className='align_center'>
+            Cart <p className="align_center cart_counts">0</p>
+            </a>
         </div>
     </nav>
   )
