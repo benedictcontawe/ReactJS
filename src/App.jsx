419267bf601872862846b8d6974589deca5d58a8
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-29 18:24:01 +0800

    update Navbar component with title and search field

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/NavBar/Navbar';
 
 const App = () => {
   return (
     <div className='app'>
-      <nav>Navbar</nav>
+      <Navbar />
       <main>All Routing</main>
     </div>
   )
