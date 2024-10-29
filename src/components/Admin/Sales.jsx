6ce67fa33a3da0ab4b44a1faab90cb1dff84e0f4
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-29 11:46:43 +0800

    add some log in Sales

--- src/components/Admin/Sales.jsx
+++ src/components/Admin/Sales.jsx
@@ -2,9 +2,12 @@ import React, { useEffect }from "react";
 
 const Sales = () => {
     useEffect( () => {
-        console.log("Sales Component Mount!")
+        console.log("Sales", "Component Mount!")
         const notification = 5;
         document.title = `Sales ${notification} new Notification`
+        return () => {
+            console.log("Sales", "Component Unmount")
+        }
     }, [])
     return <h3>Admin Sales Page</h3>;
 };
