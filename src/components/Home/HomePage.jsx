c52982b3ebf722c66bf4e999931736ca9db4062a
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-30 01:26:08 +0800

    Added Home Page

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import iphone from '../../assets/iphone-14-pro.webp'
+import mac from '../../assets/mac-system-cut.jfif'
+
+const HomePage = () => {
+  return (
+    <div>
+        <HeroSection 
+            title="Buy iPhone 14 Pro" 
+            subtitle="Experience the power of the latest 14 with our most pro camera ever."
+            link='/'
+            image={iphone}
+        />
+        {/*Featured Products*/}
+        <HeroSection 
+            title="Build the ultimate setup" 
+            subtitle="You can add Studio Display and colour-matched Magic accesories to your bag after configure your Mac mini"
+            link='/'
+            image={mac}
+        />
+    </div>
+  )
+}
+
+export default HomePage
