6da9bbe24a232e29b54bfe490b90547e04963eb0
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-30 17:29:17 +0800

    add Products Page feature add new parameter for LinkWithIcon in props for sidebar comment out HomePage first

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
 import HomePage from './components/Home/HomePage';
+import ProductsPage from './components/Products/ProductsPage'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
       <main>
-        <HomePage />
+        {/* <HomePage /> */}
+        <ProductsPage />
       </main>
     </div>
   )
