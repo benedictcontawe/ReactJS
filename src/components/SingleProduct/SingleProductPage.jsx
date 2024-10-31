c0c3d17851d2996c256a433cc91863d8d5eb44ba
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-31 13:18:37 +0800

    update SingleProductPage design ui ux

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -33,8 +33,17 @@ const SingleProductPage = () => {
             </div>
             <img src={product.images[selectedImage]} alt={product.title} className='single_product_display' />
         </div>
-        <div className="align_center single_product_details">
-
+        <div className="single_product_details">
+                <h1 className='single_product_title'>{product.title}</h1>
+                <p className='single_product_description'>{product.description}</p>
+                <p className="single_productprice">${product.price.toFixed(2)}</p>
+                <h2 className='quatity_title'>Quantity:</h2>
+                <div className="align_center quantity_input">
+                    <button className="quantity_input_button" disabled> - </button>
+                    <p className="quantity_input_count">1</p>
+                    <button className="quantity_input_button" > + </button>
+                </div>
+                <button className='search_button add_cart'>Add to Cart</button>
         </div>
     </section>
   )
