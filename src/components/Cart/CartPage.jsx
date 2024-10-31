6c090941c32a48dbca686866fe03fbb31a95b7ad
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-31 19:13:12 +0800

    Added Common Table

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './CartPage.css'
 import user from '../../assets/user.webp'
+import Table from '../Common/Table'
 
 const CartPage = () => {
   return (
@@ -10,8 +11,18 @@ const CartPage = () => {
             <p className="user_name">Harley</p>
             <p className="user_email">harley@gmail.com</p>
         </div>
-        {/* Cart Table */}
-        <table className="table cart_bill">
+        <Table headings={["Item", "Price", "Quantity", "Total", "Remove"]} >
+            <tbody>
+                <tr>
+                    <td>iPhone 14</td>
+                    <td>$999</td>
+                    <td>1</td>
+                    <td>$999</td>
+                    <td>Remove</td>
+                </tr>
+            </tbody>
+        </Table>
+        <table className="table cart_bill" >
             <tbody>
                 <tr>
                     <td>Subtotal</td>
