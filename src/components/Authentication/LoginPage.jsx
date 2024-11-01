19aa5404eaac0fec324a280a0af3ac38c8b353af
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-01 15:43:45 +0800

    added another useRef support for Name

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -2,21 +2,32 @@ import React, { useRef } from 'react'
 import './LoginPage.css'
 
 const LoginPage = () => {
-    const passordRef = useRef(null)
+    const nameRef = useRef(null)
+    const phoneRef = useRef(null)
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const user = {
+            name: "",
+            phone: 0
+        };
+        user.name = (nameRef.current.value);
+        user.phone = parseInt(phoneRef.current.value)
+        console.log(user);
+    }
   return (
     <section className='align_center form_page'>
-        <form className='authentication_form'>
+        <form className='authentication_form' onSubmit={handleSubmit}>
             <h2>Login Form</h2>
             <div className='form_inputs'>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input type='text' id='name' className='form_text_input' placeholder='Enter your name' />
+                    <input type='text' ref={nameRef} id='name' className='form_text_input' placeholder='Enter your name' />
                 </div>
                 <div>
                     <label htmlFor='phone'>Phone Number</label>
-                    <input type='password' ref={passordRef} id='phone' className='form_text_input' placeholder='Enter your phone number' />
-                    <button type='button' onClick={() => passordRef.current.type= "password"} >Hide Password</button>
-                    <button type='button' onClick={() => passordRef.current.type= "text"} >Show Password</button>
+                    <input type='number' ref={phoneRef}  id='phone' className='form_text_input' placeholder='Enter your phone number' />
+                    <button type='button' onClick={() => phoneRef.current.type= "password"} >Hide Number</button>
+                    <button type='button' onClick={() => phoneRef.current.type= "number"} >Show Number</button>
                 </div>
                 <button type='submit' className='search_button form_submit'>Submit</button>
             </div>
