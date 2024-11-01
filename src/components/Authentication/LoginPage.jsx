53309f453dadae15aab90746858bb963350c1c87
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-01 16:28:26 +0800

    Add React Hook Form Validation for Name Required and Minimum Lenght of 3

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -3,7 +3,8 @@ import { useForm } from 'react-hook-form';
 import './LoginPage.css'
 
 const LoginPage = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    
     const onSubmit = (formData) => {
         console.log(formData)
     }
@@ -19,16 +20,18 @@ const LoginPage = () => {
                         id='name' 
                         className='form_text_input' 
                         placeholder='Enter your name' 
-                        {...register("name")} />
+                        {...register("name", { required: true, minLength: 3 }) } />
+                        { errors.name?.type === "required" && <em className="form_error">Please enter your name</em> }
+                        { errors.name?.type === "minLength" && <em className="form_error">Name should be 3 or more characters</em> }
                 </div>
                 <div>
                     <label htmlFor='phone'>Phone Number</label>
                     <input 
-                        type='number' 
+                        type='number'   
                         id='phone' 
                         className='form_text_input' 
                         placeholder='Enter your phone number'
-                        {...register("phone", {valueAsNumber: true})} />
+                        { ...register("phone", {valueAsNumber: true}) } />
                 </div>
                 <button type='submit' className='search_button form_submit'>Submit</button>
             </div>
