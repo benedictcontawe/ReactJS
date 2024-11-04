84415e45308658242b732cc56a98b277a70f5031
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-04 16:03:03 +0800

    fetch Products and display to Product List

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./ProductsList.css"
 import ProductCard from './ProductCard'
+import apiClient from '../utils/api-client'
 
 const ProductsList = () => {
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState("")
+  useEffect(() => {
+    apiClient.get("/products")
+    .then(response => setProducts(response.data.products))
+    .catch(error => setError(error.message))
+  }, [])
   return (
     <section>
         <header className="align_center products_list_header">
@@ -16,12 +24,21 @@ const ProductsList = () => {
             </select>
         </header>
         <div className='products_list'>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+          {error && <em className='form_error'>{error}</em>}
+          {
+            products.map( product => 
+              <ProductCard 
+                key={product._id}
+                id={product._id}
+                image={product.images[0]}
+                price={product.price}
+                title={product.title}
+                rating={product.reviews.rate}
+                ratingCounts={product.reviews.counts}
+                stock={product.stock}
+              />
+            )
+          }
         </div>
     </section>
   )
