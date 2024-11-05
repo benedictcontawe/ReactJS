bc6d5ffe00a2a861695f8430ff4e852d254d7934
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-05 16:21:07 +0800

    update Quantity Input

--- src/components/SingleProduct/QuantityInput.jsx
+++ src/components/SingleProduct/QuantityInput.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 import './QuantityInput.css'
 
 const QuantityInput = () => {
+  const [quantity, setQuantity] = useState(1);
+  const decrementButtonRef = useRef(null);
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+  const handleDecrement = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+  useEffect(() => {
+    if (quantity <= 0) {
+      decrementButtonRef.current.disabled = true;
+    } else {
+      decrementButtonRef.current.disabled = false;
+    }
+  }, [quantity]);
   return (
     <React.Fragment>
-        <button className="quantity_input_button" disabled> - </button>
-        <p className="quantity_input_count">1</p>
-        <button className="quantity_input_button" > + </button>
+        <button className="quantity_input_button" onClick={handleDecrement} ref={decrementButtonRef} > - </button>
+        <p className="quantity_input_count">{quantity}</p>
+        <button className="quantity_input_button" onClick={handleIncrement} > + </button>
     </React.Fragment>
   )
 }
