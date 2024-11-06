f4e820227043a11d7ea290814fac03b28e16cb56
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-06 15:48:28 +0800

    Add JWT Decoder save token when is not expired, otherwise remove to local storage and refresh page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
 import Routing from './components/Routing/Routing';
+import jwtDecode from 'jwt-decode';
 
 const App = () => {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const jwtUser = jwtDecode(jwt);
+      console.log("App", jwtUser);
+      if(Date.now() >= jwtUser.exp * 1000) {
+        localStorage.removeItem("token")
+        location.reload()
+      } else {
+        setUser(jwtUser);
+      }
+    } catch (error) {
+      console.log("App error", error);
+    }
+  }, [])
   return (
     <div className='app'>
       <Navbar />
