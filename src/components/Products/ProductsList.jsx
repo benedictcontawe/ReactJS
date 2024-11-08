48dc213998369fc7056b81b1ce25e0a0f5ed866f
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-08 16:17:29 +0800

    Sort Product List based on Rate or Price using useEffect that listens on the useState of sortBy or data onChange event in select that calls setSortBy

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ProductsList.css"
 import ProductCard from './ProductCard'
 import useData from '../../hooks/useData'
@@ -7,6 +7,8 @@ import { useSearchParams } from 'react-router-dom';
 import Pagination from '../Common/Pagination';
 
 const ProductsList = () => {
+  const [sortBy, setSortBy] = useState("");
+  const [sortedProducts, setSortedProducts] = useState([]);
   const [search, setSearch] = useSearchParams();
   const category = search.get("category")
   const page = search.get("page")
@@ -24,19 +26,37 @@ const ProductsList = () => {
   useEffect(() => {
     const handleScroll = () => {
       const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
-      console.log("Scroll Top", scrollTop,"Client Height", clientHeight, "Scroll Height", scrollHeight);
+      console.log("ProductsList", "Scroll Top", scrollTop,"Client Height", clientHeight, "Scroll Height", scrollHeight);
       if(scrollTop + clientHeight >= scrollHeight - 1) {
-        console.log("Reached to Bottom!")
+        console.log("ProductsList", "Reached to Bottom!")
       }
     }
     window.addEventListener("scroll", handleScroll)
   }, [])
+  useEffect(() => {
+    console.log("ProductsList", "sortBy", sortBy)
+    const dataIsNotEmpty = data && data.products
+    if(dataIsNotEmpty) {
+      const products = [...data.products]
+      if(sortBy == "price desc") {
+        setSortedProducts(products.sort((a, b) => { return b.price - a.price }))
+      } else if (sortBy === "price asc") {
+        setSortedProducts(products.sort((a, b) => a.price - b.price ))
+      } else if (sortBy === "rate desc") {
+        setSortedProducts(products.sort((a, b) => { return b.reviews.rate - a.reviews.rate }))
+      } else if (sortBy === "rate asc") {
+        setSortedProducts(products.sort((a, b) => a.reviews.rate - b.reviews.rate))
+      } else {
+        setSortedProducts(products)
+      }
+    }
+  }, [sortBy, data])
   console.log("ProductList", data?.products, error, isLoading)
   return (
     <section>
         <header className="align_center products_list_header">
             <h2>Products</h2>
-            <select name='sort' id='products_sorting' >
+            <select name='sort' id='products_sorting' onChange={event => setSortBy(event.target.value)} >
                 <option value="">Relevance</option>
                 <option value="price desc">Price High to Low</option>
                 <option value="price asc">Price Low to High</option>
@@ -48,7 +68,7 @@ const ProductsList = () => {
           { error && <em className='form_error'>{error}</em> }
           { isLoading ? skeletons.map( skeleton => <ProductCardSkeleton key={skeleton} />) : 
             data?.products &&
-            data.products.map( product => 
+            sortedProducts.map( product => 
               <ProductCard key={product._id} product={product} />
             )
           } 
