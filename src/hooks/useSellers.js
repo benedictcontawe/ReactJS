85bb672d4a842a21c2195f60cf122c6269df0117
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-09 13:30:39 +0800

    Customize React Query Properties Query Client default options queries use Query default options properties

--- src/hooks/useSellers.js
+++ src/hooks/useSellers.js
@@ -6,7 +6,13 @@ const fetchSellers = () => apiClient.get("/users").then((response) => response.d
 const useSellers = () => {
     return useQuery({
         queryKey: ["sellers"],
-        queryFn: fetchSellers
+        queryFn: fetchSellers,
+        gcTime: 600_000,//10m
+        retry: 2,
+        staleTime: 6 * 1000,
+        refreshOnReconnect: true,
+        refreshOnMount: true,
+        refreshOnWindowFocus: true,
     });
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,18 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.jsx'
 import './index.css'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 600_000,//10m
+      retry: 3,
+      staleTime: 6 * 1000,
+      refreshOnReconnect: false,
+      refreshOnMount: false,
+      refreshOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
