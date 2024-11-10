5223e44eb8e12a92b8263f4f310b998bfe126917
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-10 15:06:45 +0800

    Optimistic Approach for adding sellers

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -9,19 +9,35 @@ const Sellers = () => {
     const { data: sellers, error, isLoading } = useSellers();
     const queryClient = useQueryClient();
     const addSellerMutation = useMutation ( {
-        mutationFn: (newSeller) => apiClient.post("/users", newSeller).then(response => response.data),
+        mutationFn: (newSeller) => apiClient.post("/userss", newSeller).then(response => response.data),
+        onMutate: (newSeller) => {
+            console.log("Sellers", "addSellerMutation", "onMutate", newSeller)
+            const previousSellers = queryClient.getQueryData(["sellers"])
+            queryClient.setQueryData(["sellers"], (sellers) => [
+                newSeller, 
+                ...sellers,
+           ])
+           return { previousSellers }
+        },
         onSuccess: (savedSeller, newSeller) => {
-            /* Method 1: Invalid cahced data
+            /* Method 1: Invalid cahced data, Not working
             queryClient.invalidateQueries({
                 queryKey: ["sellers"],
             })
-            Method 2: Update the cached data */
+            Method 2: Update the cached data, working for Pessemistic Approach
             queryClient.setQueryData(["sellers"], (sellers) => [
                 savedSeller, 
                 ...sellers,
            ])
+            */
+           queryClient.setQueriesData(["sellers", (sellers) => sellers?.map(seller => seller === newSeller ? savedSeller : seller)])
         },
-        onError: error => { console.log("Sellers", "addSellerMutation", "onError", error) }, 
+        onError: (error, newSeller, context) => { console.log("Sellers", "addSellerMutation", "onError", error) 
+            if(!context) {
+                return;
+            }
+            queryClient.setQueryData(["sellers"], context.previousSellers)
+        }, 
     } )
 
     const deleteSellerMutation = useMutation ( {
