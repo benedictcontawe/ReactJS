b91293746a68711f78cdf5dbfdf159bc1cc94beb
Ferer Atlus <benedictcontawe@gmail.com>
2024-11-11 13:29:19 +0800

    added but commented for Auto Suggestions with Caching

--- src/Network/productServices.js
+++ src/Network/productServices.js
@@ -2,4 +2,15 @@ import apiClient from "./api-client";
 
 export function getSuggestionsAPI(search) {
     return apiClient.get(`/products/suggestions?search=${search}`)
+    /*
+    const fetchProductsBySearch = () => 
+        apiClient.get(`/products/suggestions?search=${search}`).then((res) => res.data);
+     
+    return useQuery({
+    queryKey: ["products", "suggestions", search],
+    queryFn: fetchProductsBySearch,
+    staleTime: 10_000,
+    enabled: search.trim() !== "",
+    });
+    */
 }
